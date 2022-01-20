7963c40ea1f02a9397f9f2f0d39b8cb4ba4b4ea3
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-01-20 13:32:06 +0500

    Create Server.c

---
+++ Server.c
@@ -0,0 +1,17 @@
+/*
+        TCP_Client. This Program will implement the Client Side for TCP_Socket Programming.
+        It will get some data from user and will send to the server and as a reply from the
+        server, it will get its data back.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h> //socket
+#include <arpa/inet.h> //inet_addr
+
+int main(void)
+{
+        
+        
+        return 0;
+}
